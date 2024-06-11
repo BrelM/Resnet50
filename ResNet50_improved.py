8d06def61c4b4b87096e01b57044853cd10b10ad
Brel <brelfongus@gmail.com>
2024-06-11 18:18:35 +0100

    Slight modification

--- ResNet50_improved.py
+++ ResNet50_improved.py
@@ -169,9 +169,6 @@ def load_weights():
     # with open("base_model.keras", "rb") as model_file:
     #     return pickle.load(model_file)
 
-    return load_model("base_model.keras")
-    # with open("base_model.keras", "rb") as model_file:
-    #     return pickle.load(model_file)
 
 save_model(create_model())
 

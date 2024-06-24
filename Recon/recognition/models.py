185b2a6549ee2331c117dc3ac622b4cf3625fb2e
Brel <brelfongus@gmail.com>
2024-06-24 01:57:05 +0100

    Minor modifs

--- Recon/recognition/models.py
+++ Recon/recognition/models.py
@@ -9,3 +9,10 @@ class Nom(models.Model):
     nom = models.CharField(max_length=255)
     a_vote = models.BooleanField(default=False)
     char = models.CharField(max_length=int(1e10), blank=True)
+
+
+    def __str__(self) -> str:
+        return f"<Voter: {self.nom}>"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
